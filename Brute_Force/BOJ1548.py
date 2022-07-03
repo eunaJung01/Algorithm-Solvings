3d90a178a21f035f566b4fc260bbe0251a444ce4
euna <christinejung10@daum.net>
2022-07-03 20:08:53 +0900

    Fix Brute_Force - BOJ1548

--- Brute_Force/BOJ1548.py
+++ Brute_Force/BOJ1548.py
@@ -1,24 +1,30 @@
 # 부분 삼각 수열
-# 가장 큰 수 k -> k<i+j
+
+# 가장 큰 수를 z라고 했을 때 x+y>z 만족 시 삼각 수열
 
 import sys
 
 N = int(sys.stdin.readline().strip())
 numList = list(map(int, sys.stdin.readline().split()))
-numList.sort()  # 오름차순 정렬
 
 result = 0
-if len(numList) < 3:  # 숫자의 개수가 3 미만인 경우 -> 그냥 그 개수를 반환
-    result = len(numList)
+
+if N < 3:  # 숫자의 개수가 3 미만인 경우 -> N 반환
+    result = N
+
 else:
-    for i in range(len(numList) - 1):
+    numList.sort()  # 오름차순 정렬
+
+    for i in range(N - 1):
         x = numList[i]
         y = numList[i + 1]
-        for j in range(1, len(numList)):
+
+        for j in range(1, N):
             z = numList[-j]
 
             if x + y > z:
-                result = max(result, len(numList) - j - i + 1)
+                result = max(result, N - j - i + 1)
+                break
             elif y == z:
                 break
 
