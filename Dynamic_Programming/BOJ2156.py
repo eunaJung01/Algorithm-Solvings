bd91146d699fef3dd40e8b7c26ac56a438580a29
euna <christinejung10@daum.net>
2022-08-12 16:13:32 +0900

    Fix Dynamic_Programming - BOJ2156

--- Dynamic_Programming/BOJ2156.py
+++ Dynamic_Programming/BOJ2156.py
@@ -5,14 +5,17 @@ import sys
 n = int(sys.stdin.readline().strip())
 wine = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
+result = 0
 if n == 1 or n == 2:
-    print(sum(wine))
+    result = sum(wine)
 else:
     dp = [0 for _ in range(n)]
     dp[0] = wine[0]
     dp[1] = wine[0] + wine[1]
 
     for i in range(2, n):
-        dp[i] = max(dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i] + wine[i - 1])
+        dp[i] = max(dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i])
 
-    print(dp[n - 1])
+    result = dp[n - 1]
+
+print(result)
