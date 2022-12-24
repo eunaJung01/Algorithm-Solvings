c29e2a998301be3ef4906a47461de11e6fccdea9
euna <christinejung10@daum.net>
2022-12-24 20:40:25 +0900

    Add Minimum_Spanning_Tree - BOJ1922

---
+++ Minimum_Spanning_Tree/BOJ1922.py
@@ -0,0 +1,35 @@
+# 네트워크 연결
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input().strip())  # 컴퓨터의 수
+M = int(input().strip())  # 연결할 수 있는 선의 수
+
+parent = [i for i in range(N + 1)]
+
+kruskal = []
+for i in range(M):
+    a, b, c = map(int, input().split())  # a 컴퓨터, b 컴퓨터, 비용 c
+    kruskal.append((a, b, c))
+kruskal.sort(key=lambda x: x[2])  # 가중치 기준 내림차순 정렬
+
+
+def find(x):
+    if x != parent[x]:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+weight_sum = 0
+for a, b, w, in kruskal:
+    if find(a) != find(b):
+        a, b = find(a), find(b)
+        if a > b:
+            parent[a] = b
+        else:
+            parent[b] = a
+        weight_sum += w
+
+print(weight_sum)
