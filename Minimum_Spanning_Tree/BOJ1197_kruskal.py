18a7cf68a23a2262aec898a4d9ba648fdb770c25
euna <christinejung10@daum.net>
2022-12-29 11:51:49 +0900

    Fix Minimum_Spanning_Tree - BOJ1197_kruskal

--- Minimum_Spanning_Tree/BOJ1197_kruskal.py
+++ Minimum_Spanning_Tree/BOJ1197_kruskal.py
@@ -4,30 +4,31 @@ import sys
 
 input = sys.stdin.readline
 
-V, E = map(int, input().split())
+V, E = map(int, input().split())  # 노드의 개수, 간선의 개수
 parent = [i for i in range(V + 1)]
 
 kruskal = []
 for i in range(E):
-    A, B, C = map(int, input().split())  # C는 가중치
+    A, B, C = map(int, input().split())  # 노드 a, 노드 b, 가중치 w
     kruskal.append((A, B, C))
 kruskal.sort(key=lambda x: x[2])  # 가중치 기준 오름차순 정렬
 
 
+def union(a, b):
+    parent[max(a, b)] = min(a, b)
+
+
 def find(x):
     if x != parent[x]:
-        parent[x] = find(parent[x])
+        parent[x] = find(parent[x])  # 최적화를 위한 Path Compression
     return parent[x]
 
 
-weight_sum = 0
+min_weight = 0
 for a, b, w, in kruskal:
-    if find(a) != find(b):
-        a, b = find(a), find(b)
-        if a > b:
-            parent[a] = b
-        else:
-            parent[b] = a
-        weight_sum += w
+    root_a, root_b = find(a), find(b)
+    if root_a != root_b:
+        min_weight += w
+        union(root_a, root_b)
 
-print(weight_sum)
+print(min_weight)
