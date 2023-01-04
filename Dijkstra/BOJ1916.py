aa6594897838a5ccc232f5909a80e052bfe4d0fe
euna <christinejung10@daum.net>
2023-01-04 14:14:41 +0900

    Fix Dijkstra - BOJ1916

--- Dijkstra/BOJ1916.py
+++ Dijkstra/BOJ1916.py
@@ -4,9 +4,10 @@ import sys
 import heapq
 
 input = sys.stdin.readline
-INF = 2147000000
 
 N, M = int(input().strip()), int(input().strip())
+
+INF = 2147000000
 dp = [INF for _ in range(N + 1)]
 
 buses = [[] for i in range(N + 1)]
@@ -20,17 +21,17 @@ start_city, end_city = map(int, input().split())
 def dijkstra(start):
     dp[start] = 0
     heap = []
-    heapq.heappush(heap, (start, 0))
+    heapq.heappush(heap, (0, start))
 
     while heap:
-        u, w = heapq.heappop(heap)
+        w, u = heapq.heappop(heap)
         if dp[u] < w:
             continue
         for v, v_w in buses[u]:
             new_w = w + v_w
             if dp[v] > new_w:
                 dp[v] = new_w
-                heapq.heappush(heap, (v, new_w))
+                heapq.heappush(heap, (new_w, v))
 
 
 dijkstra(start_city)
