6c4c6efc7ce41dd6f1c6487a45f2669cdd62f86e
euna <christinejung10@daum.net>
2023-02-17 13:26:32 +0900

    Fix Dynamic_Programming - BOJ7570

--- Dynamic_Programming/BOJ7570.py
+++ Dynamic_Programming/BOJ7570.py
@@ -7,15 +7,18 @@ input = sys.stdin.readline
 N = int(input().strip())
 line = list(map(int, input().split()))
 
-dp = [0 for _ in range(N + 1)]
-visited = [False for _ in range(N + 1)]
+if N == 1:
+    print(0)
+else:
+    dp = [0 for _ in range(N + 1)]
+    visited = [False for _ in range(N + 1)]
 
-for num in line:
-    if num > 1:
-        if visited[num - 1]:
-            dp[num] = dp[num - 1] + 1
-        else:
-            dp[num] = 1
-        visited[num] = True
+    for num in line:
+        if num > 1:
+            if visited[num - 1]:
+                dp[num] = dp[num - 1] + 1
+            else:
+                dp[num] = 1
+            visited[num] = True
 
-print(N - max(dp))
+    print(N - max(dp))
