8e07f811f99a73ec9ff7ca1484d4da7c75fa8421
euna <christinejung10@daum.net>
2023-04-07 23:21:15 +0900

    Add Math - BOJ2525

---
+++ Math/BOJ2525.py
@@ -0,0 +1,20 @@
+# 오븐 시계
+
+import sys
+
+input = sys.stdin.readline
+
+H, M = map(int, input().split())
+timer = int(input())
+
+H += timer // 60
+M += timer % 60
+
+if M >= 60:
+    H += 1
+    M -= 60
+
+if H >= 24:
+    H -= 24
+
+print(H, M)
