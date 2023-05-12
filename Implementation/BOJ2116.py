d4320039e629268d2bab06545412533237f9c959
euna <christinejung10@daum.net>
2023-05-13 00:00:37 +0900

    Fix Implementation - BOJ2116

--- Implementation/BOJ2116.py
+++ Implementation/BOJ2116.py
@@ -4,31 +4,29 @@ import sys
 
 input = sys.stdin.readline
 
-dice_num = int(input().strip())
-dices = []
-for i in range(dice_num):
-    dices.append(list(map(int, input().split())))
-rotate = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
+N = int(input().strip())
+dices = [list(map(int, input().split())) for _ in range(N)]
+facings = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
 
-sum_result = []
-for i in range(6):
+result = []
+for first_bottomIdx in range(6):
     # 1층
-    max_list = []
     dice = [1, 2, 3, 4, 5, 6]
-    bottom = dices[0][i]
-    top = dices[0][rotate[i]]
+    bottom = dices[0][first_bottomIdx]
+    top = dices[0][facings[first_bottomIdx]]
     dice.remove(bottom)
     dice.remove(top)
-    max_list.append(max(dice))
+    maxSideNums = [max(dice)]
 
-    for j in range(1, dice_num):
-        # 2~N층
+    # 2~N층
+    for j in range(1, N):
         dice = [1, 2, 3, 4, 5, 6]
         bottom = top
+        top = dices[j][facings[dices[j].index(top)]]
         dice.remove(bottom)
-        top = dices[j][rotate[dices[j].index(top)]]
         dice.remove(top)
-        max_list.append(max(dice))
-    sum_result.append(sum(max_list))
+        maxSideNums.append(max(dice))
 
-print(max(sum_result))
+    result.append(sum(maxSideNums))
+
+print(max(result))
