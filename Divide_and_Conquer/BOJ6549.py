50e53fb5d54c676f0862e1713770a5364173c858
euna <christinejung10@daum.net>
2023-07-13 17:59:18 +0900

    Add Divide_and_Conquer - BOJ6549

---
+++ Divide_and_Conquer/BOJ6549.py
@@ -0,0 +1,37 @@
+# 히스토그램에서 가장 큰 직사각형
+
+import sys
+
+input = sys.stdin.readline
+
+
+def solve(left, right):
+    global heights
+    if left == right:
+        return heights[left]
+
+    mid = (left + right) // 2
+    result = max(solve(left, mid), solve(mid + 1, right))
+
+    p1, p2 = mid, mid + 1
+    height = min(heights[p1], heights[p2])
+    result = max(result, height * 2)
+
+    while left < p1 or p2 < right:
+        if p2 < right and (p1 == left or heights[p1 - 1] < heights[p2 + 1]):
+            p2 += 1
+            height = min(height, heights[p2])
+        else:
+            p1 -= 1
+            height = min(height, heights[p1])
+        result = max(result, height * (p2 - p1 + 1))
+
+    return result
+
+
+while True:
+    line = list(map(int, input().split()))
+    if line[0] == 0:
+        break
+    n, heights = line[0], line[1:]
+    print(solve(0, n - 1))
