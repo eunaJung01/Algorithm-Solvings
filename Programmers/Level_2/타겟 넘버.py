5dbc737f4cb5855a5256c5bec84a8fc4a4c12cb7
eunaJung01 <christinejung10@daum.net>
2024-12-10 21:54:30 +0900

    add: Programmers - 타켓 넘버

--- Programmers/Level_2/타겟 넘버.py
+++ Programmers/Level_2/타겟 넘버.py
@@ -1,3 +1,26 @@
+from collections import deque
+
+
 def solution(numbers, target):
+    q = deque()
+    q.append((numbers[0], 0))
+    q.append((-numbers[0], 0))
+
     answer = 0
+    while q:
+        total, prev_i = q.popleft()
+
+        i = prev_i + 1
+        total_plus = total + numbers[i]
+        total_minus = total - numbers[i]
+
+        if i == len(numbers) - 1:
+            if total_plus == target:
+                answer += 1
+            if total_minus == target:
+                answer += 1
+            continue
+        q.append((total_plus, i))
+        q.append((total_minus, i))
+
     return answer
